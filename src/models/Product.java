cc26c515aef61104a2fcb71032236d7552b26438
unknown <jahanshir.sm92@gmail.com>
2021-11-23 01:56:47 -0800

    add method setOffer and addCount to Product class

--- src/models/Product.java
+++ src/models/Product.java
@@ -1,9 +1,45 @@
 package models;
 
+import java.util.List;
+
 public class Product {
     private String name;
+    private int id;
     private  double price;
     private  int count;
+    private double offer;
+    private ProductPublisher productPublisher=new ProductPublisher(this);
+
+    public int getId() {
+        return id;
+    }
+
+    public double getOffer() {
+        return offer;
+    }
+
+    public List<String> setOffer(double offer) {
+        this.offer = offer;
+       return productPublisher.notifyOfferProduct();
+
+    }
+
+    public Product(int id,String name, double price, int count) {
+        this.id=id;
+        this.name = name;
+        this.price = price;
+        this.count = count;
+
+
+    }
+    public void addToPublisher(){
+        productPublisher=new ProductPublisher(this);
+        productPublisher.notifyCountProduct();
+    }
+    public List<String> addCount(int count){
+        this.count+=count;
+       return productPublisher.notifyCountProduct();
+    }
 
     public String getName() {
         return name;
@@ -27,5 +63,26 @@ public class Product {
 
     public void setCount(int count) {
         this.count = count;
+    }
+    public double calculateTotalPrice(){
+        return price-price*offer;
+    }
+
+    public ProductPublisher getProductPublisher() {
+        return productPublisher;
+    }
+
+    public void setProductPublisher(ProductPublisher productPublisher) {
+        this.productPublisher = productPublisher;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", price=" + price +
+                ", count=" + count +
+                '}';
     }
 }
