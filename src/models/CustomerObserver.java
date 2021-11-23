fb8e1f329d7a145deae0f2af8d6e5aa15b6048be
unknown <jahanshir.sm92@gmail.com>
2021-11-22 22:56:59 -0800

    add model classes in package models

---
+++ src/models/CustomerObserver.java
@@ -0,0 +1,49 @@
+package models;
+
+public class CustomerObserver {
+    private String Username;
+    private String password;
+    private Long mobileNumber;
+    private String email;
+    private String address;
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(Long mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
